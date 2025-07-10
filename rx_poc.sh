bab03912a25fe9fb77662be081e24842b56b0074
HowardPeter <phuocnt1611@gmail.com>
2025-07-10 19:03:15 +0700

    first commit

---
+++ rx_poc.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+city_name=hanoi
+curl -s wttr.in/$city_name?T --output weather_report
+
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $obs_temp"
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+
+TZ=Asia/Ho_Chi_Minh
+
+current_date=$(TZ=Asia/Ho_Chi_Minh date +'%d')
+current_month=$(TZ=Asia/Ho_Chi_Minh date +'%m')
+current_year=$(TZ=Asia/Ho_Chi_Minh date +'%Y')
+
+record=$(echo -e "$current_year\t$current_month\t$current_date\t$obs_temp\t$fc_temp C")
+echo $record>>rx_poc.log
