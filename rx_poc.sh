402d2b5be43aaf0adc057e0c0526d7ca2ac85ee3
HowardPeter <phuocnt1611@gmail.com>
2025-07-14 18:19:17 +0700

    add some echo text

--- rx_poc.sh
+++ rx_poc.sh
@@ -3,9 +3,9 @@ city_name=hanoi
 curl -s wttr.in/$city_name?T --output weather_report
 
 obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*' | cut -d "(" -f1)
-echo "The current Temperature of $city: $obs_temp"
+echo "The current Temperature of $city: $obs_temp °C"
 fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*' | cut -d "(" -f1)
-echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp °C"
 
 TZ=Asia/Ho_Chi_Minh
 
